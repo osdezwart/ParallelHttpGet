c52ab59ac184811228ddfa8702e5698dd5dafa8a
Olle de Zwart <olle@endforward.nl>
2011-02-23 23:06:04 +0100

    first commit

---
+++ ParallelHttpGet/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading;
+using Mono.Options;
+
+namespace ParallelHttpGet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var help = false;
+            string file = null;
+            string threads = null;
+            int threadCount = 5;
+
+            #region console parameter handling
+            var optionSet = new OptionSet()
+                .Add("?|help|h", "Prints out the options.", option => help = option != null)
+            .Add("file|f", "The file containing the url's to call. REQUIRED.", option => file = option)
+            .Add("threads|t", "The number of threads to use. 5 By default.", option => threads = option);
+
+            try
+            {
+                optionSet.Parse(args);
+            }
+            catch (OptionException)
+            {
+                ShowHelp("Error - usage is:", optionSet);
+            }
+
+            if (help)
+            {
+                const string usageMessage = "ParallelHttpGet.exe /f[ile] VALUE /t[hreads] VALUE ";
+                ShowHelp(usageMessage, optionSet);
+            }
+
+            if (string.IsNullOrEmpty(file))
+            {
+                ShowHelp("Error: You must specify the url file (/f).", optionSet);
+            }
+            if(!string.IsNullOrEmpty(threads))
+            {
+                int.TryParse(threads, out threadCount);
+            }
+            #endregion
+
+            var urlLines = new List<string>();
+
+            if (File.Exists(file))
+            {
+                StreamReader srFile = null;
+                try
+                {
+                    string line = null;
+                    srFile = new StreamReader(file);
+                    while ((line = srFile.ReadLine()) != null)
+                    {
+                        urlLines.Add(line);
+                    }
+                }
+                finally
+                {
+                    if (srFile != null)
+                        srFile.Close();
+                }
+            } 
+
+            var workers = new ThreadFactory(threadCount, urlLines.Count, (min, max, text) =>
+            {
+
+                for (var i = min; i <= max; i++)
+                {
+                    var request = (HttpWebRequest) WebRequest.Create("http://www.mayosoftware.com");
+                    var response = (HttpWebResponse) request.GetResponse();
+                    Thread.Sleep(1);
+                }
+            });
+
+            workers.StartWorking();
+            Console.Out.WriteLine("Completed. Press any key to continue...");
+            Console.ReadKey(true);
+        }
+
+        public static void ShowHelp(string message, OptionSet optionSet)
+        {
+            Console.Error.WriteLine(message);
+            optionSet.WriteOptionDescriptions(Console.Error);
+            Environment.Exit(-1);
+        }
+    }
+}
---
+++ ParallelHttpGet/ThreadFactory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ParallelHttpGet
+{
+    public class ThreadFactory
+    {
+        readonly int _workersCount;
+        private readonly List<Thread> _threads = new List<Thread>();
+
+        public ThreadFactory(int threadCount, int workCount, Action<int, int, string> action)
+        {
+            _workersCount = threadCount;
+
+            int totalWorkLoad = workCount;
+            int workLoad = totalWorkLoad / _workersCount;
+            int extraLoad = totalWorkLoad % _workersCount;
+
+            for (int i = 0; i < _workersCount; i++)
+            {
+                int min, max;
+                if (i < (_workersCount - 1))
+                {
+                    min = (i * workLoad);
+                    max = ((i * workLoad) + workLoad - 1);
+                }
+                else
+                {
+                    min = (i * workLoad);
+                    max = (i * workLoad) + (workLoad - 1 + extraLoad);
+                }
+                string name = "Working Thread#" + i;
+
+                var worker = new Thread(() => action(min, max, name)) {Name = name};
+                _threads.Add(worker);
+            }
+        }
+
+        public void StartWorking()
+        {
+            foreach (var thread in _threads)
+            {
+                thread.Start();
+            }
+
+            foreach (var thread in _threads)
+            {
+                thread.Join();
+            }
+        }
+    }
+}
